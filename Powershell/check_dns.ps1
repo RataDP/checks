c3b97cae97bc3a88f6e2aa1a6efca864cce03bc2
Borja Blasco <ratadp@gmail.com>
2016-11-03 15:19:06 +0100

    Allow checks with multiples results  * Se añade que pueda haber varios resultados de la resolución y lo comprueba.  * Se mejoran algunos comentarios.

--- Powershell/check_dns.ps1
+++ Powershell/check_dns.ps1
@@ -6,7 +6,7 @@
   @param:
     -name `<Nombre a resolver`>
     -dname `<Nombre para el servicio`>
-    -ip `<Ip a comparar`>
+    -r `<r a comparar`>
     -h Ayuda
 #>
 
@@ -14,31 +14,45 @@ param(
     [switch]$h = $false,
     [string]$name = $(Throw("Parameter -name is required")),
     [string]$dname = $name,
-    [string]$ip = $(Throw("Parameter -ip is required"))
+    [string]$r = $(Throw("Parameter -r is required")),
+    [string]$type = "A"
 )
 
 If ( $h ) {
-    Write-Host "Usage:`n`tcheck_dns.ps1 -name `<FQDN`> [-dname `<name_service`>] -ip `<ip_to_compare`>"
+    Write-Host "Usage:`n`tcheck_dns.ps1 -name `<FQDN`> [-dname `<name_service`>] -r `<ip_to_compare`>"
     Write-Host "`n----------------------"
     Write-Host "This program is for check Windows DNS resolution from the Check_MK Windows Agent."
     Write-Host "----------------------`n"
     Write-Host "-name `<FQDN`> `tName to test de DNS"
     Write-Host "-dname `<service_name`> `tName for the check name. If it is missing will name as -name value. It will be display in the monitor."
-    Write-Host "-ip `<ip`> `tIP to compare the results."
+    Write-Host "-r `<ip`> `tIP to compare the results."
+    Write-Host "-type `<type`> `tType of resolution name. Default is A"
     Write-Host "-help `tDisplay this message"
     Write-Host "`n----------------------`n"
     Write-Host "More infomation in https://github.com/RataDP/checks"
     Exit
 }
 
-$res = Resolve-DnsName -Name $name 2> Out-Null
+<# Fetching data #>
+$res = Resolve-DnsName -Name $name -Type $type 2> Out-Null
 
+<# Check #>
+$status = 3
+If ( $res -eq $null ) { $status = 2 } # Critical
+Else {
+  If ( $res -isnot [System.Array] ) {
+    If ( $res.IPAddress -eq $r ) { $status = 0 }
+    Else { $status = 2 }
+  } Else { # Es lista
+    Foreach ( $item in $res ) {
+      If ( $item)
+    }
 <#
     PRINT output
 #>
 # CRIT - No hay servidor DNS o no hay resolucion
 If ( $res -eq $null ) { Write-Host "2 DNSResolve-$($dname) - CRIT - $($Error[0].Exception.Message)" }
 # OK - Resolution == Esperado
-ElseIf ( $res.IPAddress -eq $ip ) { Write-Host "0 DNSResolve-$($dname) - OK - Successful resolution. Got: $($res.IPAddress)" }
+ElseIf ( $res.IPAddress -eq $r ) { Write-Host "0 DNSResolve-$($dname) - OK - Successful resolution. Got: $($res.IPAddress)" }
 # CRIT - Resolution != Esperado
-Else{ Write-Host "2 DNSResolve-$($dname) - CRIT - Wrong resolution. Got: $($res.IPAddress) Expected: $($ip)" }
+Else{ Write-Host "2 DNSResolve-$($dname) - CRIT - Wrong resolution. Got: $($res.IPAddress) Expected: $($r)" }
