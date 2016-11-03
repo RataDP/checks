40bd6852c77086b7dbd6f50ea78531f7c21b94e4
Borja Blasco <ratadp@gmail.com>
2016-11-03 15:20:10 +0100

    Added perf_data  * Se añade el perf-data al Check  * Se soluciona problema con la comprobación de paquetes pérdidos

--- Powershell/check_test_connection.ps1
+++ Powershell/check_test_connection.ps1
@@ -18,7 +18,7 @@ Else {
     If ($ping.StatusCode -eq 0) { $success++ }
   }
 
-  $packetlost = 100 - ($number/$success) * 100
+  $packetlost = $success/$number * 100
   $mean /= $res.Length
   # No hay limites
   If ( $c -eq $null -and $w -eq $null ) { $status = 0 }
@@ -29,12 +29,14 @@ Else {
 
 <# Print #>
 If  ( $c -eq $null -and $w -eq $null ) {
-  $perf_data = "time=$($mean)|packetloss=$($packetlost)"
+  $perf_data = "|rta=$($mean)ms;;; pl=$($packetlost)`%;;;"
 } Else {
-  $perf_data = "time=$($mean);$($w[0]);$($c[0])|packetloss=$($packetlost);$($w[1]);$($c[1])"
+  $perf_data = " |rta=$($mean)ms;$($w[0]);$($c[0]); pl=$($packetlost)`%;$($w[1]);$($c[1]);"
 
 }
 
-If ( $status -eq 0 ) { Write-Host "$($status) PING_$($name) $perf_data OK - Ping to $($remote) $($mean) ms. Packets lost $($packetlost)`%"}
-ElseIf ( $status -eq 1 ) { Write-Host "$($status) PING_$($name) $perf_data WARN - Ping to $($remote) $($mean) ms (W: $($w[0]) ms). Packet lost $($packetlost)`%" }
-Else { Write-Host "$($status) PING_$($name) $perf_data CRIT - Ping to $($remote) $($mean) ms!! (C: $($c[0]) ms). Packet lost $($packetlost)`%" }
+If ( $status -eq 0 ) { Write-Host "$($status) PING_$($name) OK - Ping to $($remote) $($mean) ms. Packets lost $($packetlost)`% $perf_data"}
+
+ElseIf ( $status -eq 1 ) { Write-Host "$($status) PING_$($name) WARN - Ping to $($remote) $($mean) ms (W: $($w[0]) ms). Packet lost $($packetlost)`% $perf_data" }
+
+Else { Write-Host "$($status) PING_$($name) CRIT - Ping to $($remote) $($mean) ms!! (C: $($c[0]) ms). Packet lost $($packetlost)`% $perf_data" }
